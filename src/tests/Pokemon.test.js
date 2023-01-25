b49b82082a403853afd19f03d4095610bdd8ff51
Gabriel Lemos <jmjgb@outlook.com>
2023-01-25 04:16:53 -0300

    Requisito 7 finalizado

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('É exibido na tela um h2 com o texto <name> Details', () => {
+  renderWithRouter(<App />);
+  const button = screen.getByRole('link', { name: /more details/i });
+  const pokemonName = screen.getByTestId('pokemon-name').innerHTML;
+  fireEvent.click(button);
+  const h2 = screen.getByRole('heading', { name: `${pokemonName} Details` });
+  expect(h2).toHaveTextContent(`${pokemonName} Details`);
+  //console.log(h2.innerHTML)
+});
+
+test('É exibido na tela um h2 com o texto Summary', () => {
+  renderWithRouter(<App />);
+  const button = screen.getByRole('link', { name: /more details/i });
+  fireEvent.click(button);
+  const h2 = screen.getByRole('heading', { name: /summary/i });
+  expect(h2).toBeInTheDocument();
+  expect(h2).toHaveTextContent('Summary');
+});
+
+test('É exibido na tela um texto contendo <summary>', () => {
+  renderWithRouter(<App />);
+  const button = screen.getByRole('link', { name: /more details/i });
+  fireEvent.click(button);
+  const h2 = screen.getByRole('heading', { name: /summary/i });
+  const summary = h2.nextElementSibling;
+  expect(summary).toHaveTextContent(summary.innerHTML);
+});
+
+test('É exibido na tela um h2 com o texto Game Locations of <name>', () => {
+  renderWithRouter(<App />);
+  const button = screen.getByRole('link', { name: /more details/i });
+  fireEvent.click(button);
+  const pokemonName = screen.getByTestId('pokemon-name').innerHTML;
+  const gameLocation = screen.getByRole('heading', { name: `Game Locations of ${pokemonName}` });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { updateFavoritePokemon } from '../services/pokedexService';
+import App from '../App';
+
+test('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela', () => {
+  renderWithRouter(<App />);
+  const moreDetailsButton = screen.getByRole('link', { name: /more details/i });
+  const pokemonName = screen.getByTestId('pokemon-name').innerHTML;
+  fireEvent.click(moreDetailsButton);
+  expect(screen.getByText(`${pokemonName} Details`)).toBeInTheDocument();
+  expect(moreDetailsButton).not.toBeInTheDocument();
+  expect(moreDetailsButton).not.toBeVisible();
+  expect(screen.getByText('Summary')).toBeInTheDocument();
+  const summary = screen.getByText('Summary').nextElementSibling.innerHTML;
+  expect(screen.getByText(`${summary}`)).toBeInTheDocument();
+});
+
+test('Teste se existe na página uma seção com os mapas contendo as localizações do Pokémon', () => {
+  renderWithRouter(<App />);
+  const moreDetailsButton = screen.getByRole('link', { name: /more details/i });
+  const pokemonName = screen.getByTestId('pokemon-name').innerHTML;
+  fireEvent.click(moreDetailsButton);
+  expect(screen.getByText(`Game Locations of ${pokemonName}`)).toBeInTheDocument();
+  const locationImages = screen.getAllByAltText(`${pokemonName} location`);
+  locationImages.forEach((image) => {
+    expect(image).toHaveAttribute('src', image.src);
+  });
+});
+
+test('Teste se o usuário pode favoritar um Pokémon através da página de detalhes', () => {
+  renderWithRouter(<App />);
+  const moreDetailsButton = screen.getByRole('link', { name: /more details/i });
+  fireEvent.click(moreDetailsButton);
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).toBeInTheDocument();
+  expect(screen.getByLabelText('Pokémon favoritado?'));
+  fireEvent.click(checkbox);
+  const star = screen.getByAltText('Pikachu is marked as favorite');
+  expect(star).toBeInTheDocument();
+  fireEvent.click(checkbox);
+  expect(star).not.toBeInTheDocument();
+});
